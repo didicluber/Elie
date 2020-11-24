00aebc3e243f36d33c489ead82ca0371a09ba232
dylan <dylanouanounou@yahoo.fr>
2020-11-24 19:00:51 +0100

    2

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let http = require('http');
-let fs = require('fs');
+//1.
+var http = require('http');
+var fs = require('fs');
+//2.
+var server = http.createServer(function (req, resp) {
+    //3.
+    if (req.url === "/create") {
+        fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('file not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
+            resp.end();
+        });
+    } else {
+        //4.
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
+});
+//5.
+server.listen(5050);
 
-http.createServer(handleRequest).listen(3500);
+console.log('Server Started listening on 5050');
