3e0f93ce6c6bfe526d57f0d65adca4f0ee018cea
SIGAL <SIGAL@intech-s1>
2020-12-01 16:42:14 +0100

    dylan 2

---
+++ premier.js
@@ -0,0 +1,32 @@
+//1. importation des modules
+var http = require('http');
+var fs = require('fs');
+const port = 3000;
+//2. Creation du serveur 
+var server = http.createServer(function (req, resp) {
+
+//3. Ecriture de la page sur le navigateur
+    if (req.url === "/") {
+        fs.readFile("accueil.html", function (erreur, pgResp) {                   if (erreur) {
+
+                resp.writeHead(404);
+                resp.write('contenu non trouver');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+
+            resp.end();
+        });
+    } else {
+
+//4.  on indiquee au navigateur que c'est du html
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.end();
+    }
+});
+
+//5. ecoute du port
+server.listen(port);
+
+console.log(`Serveur: localhost:${port}`)
